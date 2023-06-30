bcbb18883b3a0f38c425947e1f361d438bc5e98d
KgMLe <kgodisholebopa@gmail.com>
2023-06-30 16:26:14 +0200

    Layout

---
+++ src/components/AbOut.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <p>Hi, I'm Kgodisho, an aspiring Cloud User Experience Developer. I am currently a student at Life Choices coding academy gaining skills in full stack web development. I enjoy creating beautiful, responsive websites that provide an excellent user experience. I am fueled by my passion for understanding the nuances of the advancing world of Technology. I considers myself a "forever student," eager to both build on my academic foundations in systems development by staying in tune with the latest tech trends through continued coursework and professional development.
+In my free time, I enjoy both abstract and street photography and all things that involve art and being creative
+
+</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
